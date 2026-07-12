8e1363d5f9a8fd5852e80544dadb1801a03db529
KazuProg <91452566+KazuProg@users.noreply.github.com>
2026-07-13 01:33:17 +0900

    refactor: extract shared message type and origin URL constants

---
+++ chrome-extension/js/constants.js
@@ -0,0 +1,2 @@
+const YTVJ_MESSAGE_TYPE_NOTIFY_VIDEO_ID = "YTVJ_EXTENSION_NOTIFY_VIDEO_ID";
+const YTVJ_ORIGIN = "https://youtube-vj.kazuprog.work/";
--- chrome-extension/js/vj-main.js
+++ chrome-extension/js/vj-main.js
@@ -2,7 +2,7 @@
   window.addEventListener("message", (event) => {
     if (
       event.source === window &&
-      event.data.type === "YTVJ_EXTENSION_NOTIFY_VIDEO_ID" &&
+      event.data.type === YTVJ_MESSAGE_TYPE_NOTIFY_VIDEO_ID &&
       "mixer" in window
     ) {
       window.mixer.setPreparedVideo(event.data.data);
