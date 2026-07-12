d8781a6fe9df91c770ae498f28041ff43a6863e3
KazuProg <91452566+KazuProg@users.noreply.github.com>
2026-07-13 01:33:17 +0900

    refactor: extract shared message type and origin URL constants

--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -3,7 +3,7 @@
     const btn = document.querySelector("#open_windows");
     btn.addEventListener("click", () => {
       chrome.windows.create({
-        url: "https://youtube-vj.kazuprog.work/",
+        url: YTVJ_ORIGIN,
         type: "popup",
         width: 960,
         height: 540,
